f4479b2d4a81cd28fe6302dd0b671f4ed625b8f1
arielEliasPerez <ariel65_ep@hotmail.com>
2024-12-19 15:36:02 -0300

    correcion error en search.js

--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,10 @@
 
-import { menuData } from "../data/menuData.js";
+import { menuDatas } from "../data/menuData.js";
 import { renderMenu } from "./menu.js";
 
 document.getElementById("search-input").addEventListener("input", event => {
   const query = event.target.value.toLowerCase();
-  const filteredData = menuData.filter(item =>
+  const filteredData = menuDatas.filter(item =>
     item.name.toLowerCase().includes(query)
   );
 
